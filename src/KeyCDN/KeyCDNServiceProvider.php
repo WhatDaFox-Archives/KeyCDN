847ff09989dd31e097049b338fc6a2433fb71794
Valentin Prugnaud <valentin@whatdafox.com>
2017-03-08 03:51:10 -0800

    Add Laravel Service Provider

---
+++ src/KeyCDN/KeyCDNServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace KeyCDN;
+
+use Illuminate\Support\ServiceProvider;
+use KeyCDN\Facades\KeyCDN;
+use KeyCDN\Facades\KeyCDNFacade;
+
+/**
+ * Class KeyCDNServiceProvider
+ * @package KeyCDN
+ */
+class KeyCDNServiceProvider extends ServiceProvider
+{
+    /**
+     * Register config files
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/keycdn.php' => config_path('keycdn.php'),
+        ], 'keycdn');
+    }
+
+    /**
+     * Register Service
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/keycdn.php', 'keycdn'
+        );
+
+        $this->app->bind(KeyCDN::class, function() {
+            return KeyCDN::create(config('keycdn.key'));
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     * @return array
+     */
+    public function provides()
+    {
+        return array('keycdn');
+    }
+}
---
+++ src/config/keycdn.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /**
+     * Your KeyCDN API key
+     */
+    'key' => env('KEYCDN_KEY', 'YOUR_KEY_CDN_API_KEY'),
+
+    /**
+     * Zones
+     */
+    'zones' => [
+        'cdn' => [
+            'id' => 0
+        ]
+    ]
+];
